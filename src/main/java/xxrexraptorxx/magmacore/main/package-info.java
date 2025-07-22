9677291dc61dda273d4e2bcf2d98413284231af7
XxRexRaptorxX <Raptor-Simon@web.de>
2025-07-22 21:06:15 +0200

    chore: automations added

--- src/main/java/xxrexraptorxx/magmacore/main/package-info.java
+++ src/main/java/xxrexraptorxx/magmacore/main/package-info.java
@@ -1,5 +1,5 @@
 /**
  * @author XxRexRaptorxX (RexRaptor)
- * @projectPage @projectPage https://www.curseforge.com/minecraft/mc-mods/magma-core *
+ * @projectPage https://www.curseforge.com/minecraft/mc-mods/magma-core
  */
 package xxrexraptorxx.magmacore.main;
